f4940a395a936889d2a70e396512a042921e0707
Buckley212 <62124782+Buckley212@users.noreply.github.com>
2020-04-15 16:49:14 -0400

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,12 +1,13 @@
 import random
 
-# Getting Computer's number
-
-# Getting player's number
-# Deciding if you guessed correctly
+# Game Loop
 def pick_winner():
+    
+    # Getting the player's number
     player_guess = input("Please input a number from 1 to 5: \n")
-    player_number = int(player_guess)
+    player_number = int(player_guess) #Turning it into an integer
+    
+    
     computer_number = random.randint(1, 6)
 
     if player_number <6 and player_number > 0 and player_number == computer_number:
